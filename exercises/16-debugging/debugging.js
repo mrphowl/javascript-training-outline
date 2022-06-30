fd92fc29c48ac65ce41c7e8a2f1ad1908128b7ae
Paul Adraneda <pegadraneda@gmail.com>
2022-06-30 19:00:29 +0800

    Debugging Tools
    - Different logging in the console
      - log
      - info
      - error
      - warn
      - table
      - count
      - group
      - groupCollapsed
      - groupEnd
    - Call stack and call trace

---
+++ exercises/16-debugging/debugging.js
@@ -0,0 +1,76 @@
+/* eslint-disable */
+const people = [
+  { name: 'Wes', cool: true, country: 'Canada' },
+  { name: 'Scott', cool: true, country: 'Merica' },
+  { name: 'Snickers', cool: false, country: 'Dog Country' },
+];
+
+people.forEach((person, index) => {
+  console.groupCollapsed(`${person.name}`);
+  console.log(person.country);
+  console.log(person.cool);
+  console.log("DONE!!");
+  console.groupEnd(`${person.name}`);
+});
+
+// Console Methods
+function doALotOfStuff() {
+  console.group('Doing some stuff');
+  console.log('log');
+  console.warn('warn');
+  console.error('error');
+  console.groupEnd('Doing some stuff');
+}
+// Callstack
+function bootstrap() {
+  go();
+}
+bootstrap();
+// Grabbing Elements
+
+// Breakpoints
+
+// Scope
+
+// Network Requests
+
+// Break On Attribute
+
+// Some Setup Code
+
+function doctorize(name) {
+  // console.count() is based on what strings you pass it
+  // console.count(`running doctorize for ${name}`); // Counts separately for each name
+  return `Dr. ${name}`;
+}
+
+function greet(name) {
+  doesntExist();
+  return `Hello ${name}`;
+}
+
+function go() {
+  const name = doctorize(greet('Wes'));
+  console.log(name);
+}
+
+const button = document.querySelector('.bigger');
+button.addEventListener('click', function(e) {
+  const newFontSize =
+    parseFloat(getComputedStyle(e.currentTarget).fontSize) + 1;
+  e.currentTarget.style.fontSize = `${newFontSize}px`;
+});
+
+// A Dad joke fetch
+async function fetchDadJoke() {
+  const res = await fetch('https://icanhazdadjoke.com/', {
+    headers: {
+      Accept: 'text/plain',
+    },
+  });
+  const joke = await res.text();
+  console.log(joke);
+  return joke;
+}
+
+
