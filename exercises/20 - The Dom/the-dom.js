32cc041deb4d4e199014a4405072e05ba0bfd257
Paul Adraneda <pegadraneda@gmail.com>
2022-07-06 15:04:08 +0800

    Work around to load the Javascript in the <haed> tag and make it work.

--- exercises/20 - The Dom/the-dom.js
+++ exercises/20 - The Dom/the-dom.js
@@ -7,6 +7,13 @@
  * run before the HTML or the actual elements are created.
  *
  * One way around this is to load the Javascript before the closing <body> tag. (see index.html)
+ *
+ * Another way is to put all the js code inside a function, and then add an event listener that listens for
+ * the DOM content loaded event and runs the init() function below. With this, we can move the script src back inside
+ * the <head> tag.
  */
-const p = document.querySelector('p');
-console.log(p);
+function init() {
+  const p = document.querySelector('p');
+  console.log(p);
+}
+document.addEventListener('DOMContentLoaded', init);
