f78d7c1fa58e24fb20f6bfa6dd40802725b7c345
Paul Adraneda <pegadraneda@gmail.com>
2022-07-11 15:33:12 +0800

    Built-in attributes
    - Setting and getting attribute values
    - Use dot notation to access attributes
    - Add and event listener for the load event to get the accurate data from your page
    - getAttribute, setAttribute, hasAttribute, and their difference from the dot notation

--- exercises/20 - The Dom/the-dom.js
+++ exercises/20 - The Dom/the-dom.js
@@ -1,6 +1,6 @@
 /**
  * JS-201: The DOM
- * 
+ *
  * Working wih Classes
  */
 /* eslint no-console: 0 */
@@ -15,61 +15,55 @@ pic.classList.toggle('round');
 console.log('--img.nice add round', pic.classList);
 
 function toggleRound() {
-    pic.classList.toggle('round');
+  pic.classList.toggle('round');
 }
 
 pic.addEventListener('click', toggleRound);
 
-const fxClass = [
-    'round',
-    'rotate-1turn',
-    'rotate-20deg',
-    'rotate-2kdeg',
-    'rotate1-scale2'
-];
+const fxClass = ['round', 'rotate-1turn', 'rotate-20deg', 'rotate-2kdeg', 'rotate1-scale2'];
 
 /**
  * Pick a class from fxClass and return it with the last chosen class.
  *
  * Yey, private variable!
- * 
+ *
  * @returns string
  */
 function randomEffects() {
-    let currentEffect = '';
+  let currentEffect = '';
 
-    return function applyNew() {
-        let newEffect = '';
-        // Using do-while to prevent the same choice as the currentEffect
-        do {
-            // pick any one item from fxClass as the new effect
-            const idx = Math.floor(Math.random() * fxClass.length);
-            newEffect = fxClass[idx];
-        } while(newEffect === currentEffect);
-        // we will return the old and new class after we...
-        const effects = [currentEffect, newEffect];
-        // ...set newEffect as the new currentEffect
-        currentEffect = newEffect;
-        return effects;
-    };
+  return function applyNew() {
+    let newEffect = '';
+    // Using do-while to prevent the same choice as the currentEffect
+    do {
+      // pick any one item from fxClass as the new effect
+      const idx = Math.floor(Math.random() * fxClass.length);
+      newEffect = fxClass[idx];
+    } while (newEffect === currentEffect);
+    // we will return the old and new class after we...
+    const effects = [currentEffect, newEffect];
+    // ...set newEffect as the new currentEffect
+    currentEffect = newEffect;
+    return effects;
+  };
 }
 
 const effect = randomEffects();
 
 function toggleEffect() {
-    let [oldClass, newCLass] = effect();
+  const [oldClass, newCLass] = effect();
 
-    // use the contains method to check if the element has a specific class
-    if(oldClass && pic.classList.contains(oldClass)) {
-        // we have a class to remove from pic
-        pic.classList.remove(oldClass);
-    }
+  // use the contains method to check if the element has a specific class
+  if (oldClass && pic.classList.contains(oldClass)) {
+    // we have a class to remove from pic
+    pic.classList.remove(oldClass);
+  }
 
-    pic.classList.toggle(newCLass);
+  pic.classList.toggle(newCLass);
 }
 
 function removeAllEffects() {
-    pic.classList.remove(...fxClass);
+  pic.classList.remove(...fxClass);
 }
 
 // button controls
@@ -78,3 +72,32 @@ randomBtn.addEventListener('click', toggleEffect);
 
 const resetBtn = document.querySelector('.btn-reset');
 resetBtn.addEventListener('click', removeAllEffects);
+
+/**
+ * Built-in and Custom Data Attributes
+ *
+ * All attributes on an element are done via getters and setters.
+ * You can use the dot notation to access them.
+ */
+// The load event waits for all the images, resources, CSS, JavaScript, and anything that needs to be loaded to load
+// before calling the function.
+window.addEventListener('load', () => {
+  // naturalWidth is only a getter; it can not be changed
+  console.log('--pic.naturalWidth:', pic.naturalWidth);
+});
+
+// The event listener can also be used on a specific image.
+pic.addEventListener('load', () => {
+  console.log('--pic-load-event: pic.naturalWidth', pic.naturalWidth);
+});
+
+// Get the alt attribute
+console.log("--pic.getAttribute('alt')", pic.getAttribute('alt'));
+
+// set the alt attribute
+pic.setAttribute('alt', 'I am a new alt.');
+console.log("--pic.getAttribute('alt') new value", pic.getAttribute('alt'));
+
+// check if pic has this attribute
+console.log("--pic.hasAttribute('title')?", pic.hasAttribute('title'));
+console.log("--pic.hasAttribute('width')?", pic.hasAttribute('width'));
