00a62c7831737894437a0f427b7b74fd3c368952
Paul Adraneda <pegadraneda@gmail.com>
2022-07-20 20:29:01 +0800

    Using Intersection Observer

--- exercises/35 - Scroll To Accept/scroll-to-accept.js
+++ exercises/35 - Scroll To Accept/scroll-to-accept.js
@@ -1,6 +1,7 @@
 /**
  * JS-202: Events And Triggers - Scroll Events and Intersection Observer
  */
+const button = document.querySelector('.accept');
 function scrollToAccept() {
   const terms = document.querySelector('.terms-and-conditions');
 
@@ -8,10 +9,25 @@ function scrollToAccept() {
     return;
   }
 
-  terms.addEventListener('scroll', e => {
-    console.log(e.currentTarget.scrollTop);
-    console.log(e.currentTarget.scrollHeight);
+  function obCallback(payload) {
+    if (payload[0].intersectionRatio === 1) {
+      button.disabled = false;
+      // stop observing the button
+      ob.unobserve(terms.lastElementChild);
+    }
+  }
+
+  const ob = new IntersectionObserver(obCallback, {
+    root: terms,
+    threshold: 1,
   });
+
+  ob.observe(terms.lastElementChild);
+
+  // terms.addEventListener('scroll', e => {
+  //   console.log(e.currentTarget.scrollTop);
+  //   console.log(e.currentTarget.scrollHeight);
+  // });
 }
 
 scrollToAccept();
